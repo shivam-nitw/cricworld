51a040b123ac7a84c441eab88225c35fc7a3b24b
spor3006 <sparshalkothari2014@gmail.com>
2021-04-11 00:14:48 +0530

    deployment changes

--- Cricworld/settings.py
+++ Cricworld/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +130,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets' )
+
+django_heroku.settings(locals())
